20b52c7635e6678a23b13d9c0158e4286675ccf1
Isha Malla <malla.isha807@gmail.com>
2017-01-19 18:24:19 -0800

    Update AppBasics.java and Create NumberNotInRangeException.java

---
+++ src/main/java/com/sqa/im/helpers/exceptions/NumberNotInRangeException.java
@@ -0,0 +1,25 @@
+/**
+ *   File Name: NumberNotInRangeException.java<br>
+ *
+ *   Malla, Isha<br>
+ *   Created: Jan 18, 2017
+ *   
+ */
+
+package com.sqa.im.helpers.exceptions;
+
+/**
+ * NumberNotInRangeException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Malla, Isha
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class NumberNotInRangeException extends Exception {
+
+}
